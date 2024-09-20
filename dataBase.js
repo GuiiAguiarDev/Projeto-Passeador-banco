decfddb4bd197d61eb8a83b2f27bd0017ecb36c9
GuiiAguiarDev <142525726+GuiiAguiarDev@users.noreply.github.com>
2024-09-20 16:45:58 -0300

    atualização nova 20/09/2024

--- dataBase.js
+++ dataBase.js
@@ -1,3 +1,4 @@
+/*Cadastrando informações no banco */
 "use strict";
 const readClient = () => getLocalStorage();
 
@@ -22,13 +23,81 @@ const saveClient = () => {
     cor: document.getElementById("cor").value,
 
     idade: document.getElementById("idade").value,
+    //Mesmo nao tendo o campo estou vinculando o meu salvamento no banco com o usuario que estou logado
+    user:{
+      uid: firebase.auth().currentUser.email
+    } 
   };
 
 db.collection("cachorro").add({
     client:client
 });
 
+db.collection('cachorro').onSnapshot(function(data){
+
+  let list = document.querySelector('#tableClient>tbody');
+
+  data.docs.map(function(val){
+      list.innerHTML+=`<td>${val.data().client.nome}</td>`+`<td>${val.data().client.raca}</td>`+`<td>${val.data().client.cor}</td>`+`<td>${val.data().client.idade}</td>`+`<td>${val.data().client.user.uid}</td>`;
+  
+    
+  })
   createClient(client);
+  updatetable();
+})
+
+ 
 };
 
+
+const createRow = (client, index) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>${client.nome}</td>
+  <td>${client.raca}</td>
+  <td>${client.cor}</td>
+  <td>${client.idade}</td>
+  <td>${client.user.uid}</td>
+
+  <td>
+ 
+  `;
+
+  //Arruma depois aqui ou la em cima
+  document.querySelector("#tableClient>tbody").appendChild(newRow);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+const updatetable = () => {
+  const dbClient = readClient();
+  //Depois que atualizar limpar tabela
+  dbClient.forEach(createRow);
+};
+
+
+
+
 document.getElementById("salvar").addEventListener("click", saveClient);
+/*Cadastrando informações no banco */
+
