48ce6c9274931e9b8cf11038b132de584bb1de9a
GuiiAguiarDev <142525726+GuiiAguiarDev@users.noreply.github.com>
2024-10-25 11:15:14 -0300

    foi atualização

--- registrarLogin.js
+++ registrarLogin.js
@@ -1,19 +1,5 @@
-"use strict";
-const readClient = () => getLocalStorage();
-
-const getLocalStorage = () =>
-  JSON.parse(localStorage.getItem("db_client")) ?? [];
-const setLocalStorage = (db_client) =>
-  localStorage.setItem("db_client", JSON.stringify(db_client));
-
-const createClient = (client) => {
-  const dbClient = getLocalStorage();
-  dbClient.push(client);
-  setLocalStorage(dbClient);
-};
-
 //Cadastrar Usuario
-function cadastrar() {
+async function cadastrar() {
   const client = {
     usuario: document.getElementById("criacaoUsuario").value,
     senha: document.getElementById("criacaoSenha").value,
@@ -22,22 +8,18 @@ function cadastrar() {
     //Pegando as in
     tipo: document.querySelector("input[name='tipo']:checked").value,
   };
-  if (client.senha === client.senha1 && client.tipo === "cliente") {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(client.usuario, client.senha)
-      .then(() => {
-        window.location.href = "login.html";
-      })
-      .catch((error) => {
-        alert(getErrorMessage(error));
-      });
 
-    db.collection("users").add({
+  try {
+    const newUser = await this.firebase
+      .auth()
+      .createUserWithEmailAndPassword(client.usuario, client.senha);
+
+    db.collection("users").doc(newUser.user.uid).set({
       client: client,
     });
-  } else {
-    console.log("erro");
-    window.location.href = "www.google.com.br";
+
+    console.log(newUser);
+  } catch (error) {
+    console.log(error);
   }
 }
